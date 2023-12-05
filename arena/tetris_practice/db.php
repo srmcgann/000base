b7204325e4292e17979e0d258a2901601027ce60
scott mcgann <s.r.mcgann@hotmail.com>
2023-12-05 22:49:26 +0000

    sync

---
+++ arena/tetris_practice/db.php
@@ -0,0 +1,8 @@
+<?php
+  $db_user="id21616125_user";
+  $db_pass="Chrome57253!*";
+  $db_host="localhost";
+  $db="id21616125_multiplayer";
+  $baseURL='whr1.000webhostapp.com/b/games/tetris';
+  $link = mysqli_connect($db_host, $db_user, $db_pass, $db);
+?>
